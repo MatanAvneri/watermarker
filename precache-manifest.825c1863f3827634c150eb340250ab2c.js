45d5fac7d11e9bec2b164c5a75528c4f395f6cfc
matanavneri <matan.av@gmail.com>
2020-08-24 09:07:11 +0300

    Updates

---
+++ precache-manifest.825c1863f3827634c150eb340250ab2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327afb730c0fbe58fada5bcdc91729c9",
+    "url": "/watermarker/index.html"
+  },
+  {
+    "revision": "559c9ffd8dacdd686464",
+    "url": "/watermarker/static/css/main.32ba10b8.chunk.css"
+  },
+  {
+    "revision": "ccb15dda13e71607b744",
+    "url": "/watermarker/static/js/2.212e815b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watermarker/static/js/2.212e815b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559c9ffd8dacdd686464",
+    "url": "/watermarker/static/js/main.aeec4247.chunk.js"
+  },
+  {
+    "revision": "414c514547da0b81a84e",
+    "url": "/watermarker/static/js/runtime-main.6752172d.js"
+  }
+]);
